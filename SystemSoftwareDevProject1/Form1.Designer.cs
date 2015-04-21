158879efe67e313ec2e5efe1de7e568981eb770d
Blake Burton <b.burton1@verizon.net>
2015-04-20 21:03:09 -0400

    Made the start date to  1/1/1900
    project instructions say need to be that dote or lose a lot of points

--- SystemSoftwareDevProject1/Form1.Designer.cs
+++ SystemSoftwareDevProject1/Form1.Designer.cs
@@ -52,20 +52,20 @@ namespace SystemSoftwareDevProject1
             // lbFromDateLabel
             // 
             this.lbFromDateLabel.AutoSize = true;
-            this.lbFromDateLabel.Location = new System.Drawing.Point(9, 23);
-            this.lbFromDateLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbFromDateLabel.Location = new System.Drawing.Point(18, 44);
+            this.lbFromDateLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbFromDateLabel.Name = "lbFromDateLabel";
-            this.lbFromDateLabel.Size = new System.Drawing.Size(59, 13);
+            this.lbFromDateLabel.Size = new System.Drawing.Size(118, 25);
             this.lbFromDateLabel.TabIndex = 3;
             this.lbFromDateLabel.Text = "From Date:";
             // 
             // lbToDateLabel
             // 
             this.lbToDateLabel.AutoSize = true;
-            this.lbToDateLabel.Location = new System.Drawing.Point(16, 42);
-            this.lbToDateLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbToDateLabel.Location = new System.Drawing.Point(32, 81);
+            this.lbToDateLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbToDateLabel.Name = "lbToDateLabel";
-            this.lbToDateLabel.Size = new System.Drawing.Size(49, 13);
+            this.lbToDateLabel.Size = new System.Drawing.Size(94, 25);
             this.lbToDateLabel.TabIndex = 5;
             this.lbToDateLabel.Text = "To Date:";
             // 
@@ -73,21 +73,21 @@ namespace SystemSoftwareDevProject1
             // 
             this.gbLocality.Controls.Add(this.rbView);
             this.gbLocality.Controls.Add(this.rbDownload);
-            this.gbLocality.Location = new System.Drawing.Point(89, 85);
-            this.gbLocality.Margin = new System.Windows.Forms.Padding(2);
+            this.gbLocality.Location = new System.Drawing.Point(178, 163);
+            this.gbLocality.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gbLocality.Name = "gbLocality";
-            this.gbLocality.Padding = new System.Windows.Forms.Padding(2);
-            this.gbLocality.Size = new System.Drawing.Size(100, 61);
+            this.gbLocality.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbLocality.Size = new System.Drawing.Size(200, 117);
             this.gbLocality.TabIndex = 7;
             this.gbLocality.TabStop = false;
             // 
             // rbView
             // 
             this.rbView.AutoSize = true;
-            this.rbView.Location = new System.Drawing.Point(3, 34);
-            this.rbView.Margin = new System.Windows.Forms.Padding(2);
+            this.rbView.Location = new System.Drawing.Point(6, 65);
+            this.rbView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbView.Name = "rbView";
-            this.rbView.Size = new System.Drawing.Size(48, 17);
+            this.rbView.Size = new System.Drawing.Size(89, 29);
             this.rbView.TabIndex = 1;
             this.rbView.TabStop = true;
             this.rbView.Text = "View";
@@ -96,10 +96,10 @@ namespace SystemSoftwareDevProject1
             // rbDownload
             // 
             this.rbDownload.AutoSize = true;
-            this.rbDownload.Location = new System.Drawing.Point(3, 16);
-            this.rbDownload.Margin = new System.Windows.Forms.Padding(2);
+            this.rbDownload.Location = new System.Drawing.Point(6, 31);
+            this.rbDownload.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbDownload.Name = "rbDownload";
-            this.rbDownload.Size = new System.Drawing.Size(73, 17);
+            this.rbDownload.Size = new System.Drawing.Size(138, 29);
             this.rbDownload.TabIndex = 0;
             this.rbDownload.TabStop = true;
             this.rbDownload.Text = "Download";
@@ -107,20 +107,21 @@ namespace SystemSoftwareDevProject1
             // 
             // dpFromDate
             // 
-            this.dpFromDate.Location = new System.Drawing.Point(71, 23);
-            this.dpFromDate.Margin = new System.Windows.Forms.Padding(2);
+            this.dpFromDate.Location = new System.Drawing.Point(142, 44);
+            this.dpFromDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dpFromDate.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
             this.dpFromDate.Name = "dpFromDate";
-            this.dpFromDate.Size = new System.Drawing.Size(190, 20);
+            this.dpFromDate.Size = new System.Drawing.Size(376, 31);
             this.dpFromDate.TabIndex = 8;
+            this.dpFromDate.Value = new System.DateTime(1900, 1, 1, 21, 1, 0, 0);
             // 
             // dpToDate
             // 
-            this.dpToDate.Location = new System.Drawing.Point(71, 42);
-            this.dpToDate.Margin = new System.Windows.Forms.Padding(2);
+            this.dpToDate.Location = new System.Drawing.Point(142, 81);
+            this.dpToDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dpToDate.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
             this.dpToDate.Name = "dpToDate";
-            this.dpToDate.Size = new System.Drawing.Size(190, 20);
+            this.dpToDate.Size = new System.Drawing.Size(376, 31);
             this.dpToDate.TabIndex = 9;
             // 
             // gbResolution
@@ -128,21 +129,21 @@ namespace SystemSoftwareDevProject1
             this.gbResolution.Controls.Add(this.rbMonth);
             this.gbResolution.Controls.Add(this.rbWeek);
             this.gbResolution.Controls.Add(this.rbDay);
-            this.gbResolution.Location = new System.Drawing.Point(12, 79);
-            this.gbResolution.Margin = new System.Windows.Forms.Padding(2);
+            this.gbResolution.Location = new System.Drawing.Point(24, 152);
+            this.gbResolution.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gbResolution.Name = "gbResolution";
-            this.gbResolution.Padding = new System.Windows.Forms.Padding(2);
-            this.gbResolution.Size = new System.Drawing.Size(64, 68);
+            this.gbResolution.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbResolution.Size = new System.Drawing.Size(128, 131);
             this.gbResolution.TabIndex = 10;
             this.gbResolution.TabStop = false;
             // 
             // rbMonth
             // 
             this.rbMonth.AutoSize = true;
-            this.rbMonth.Location = new System.Drawing.Point(4, 50);
-            this.rbMonth.Margin = new System.Windows.Forms.Padding(2);
+            this.rbMonth.Location = new System.Drawing.Point(8, 96);
+            this.rbMonth.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbMonth.Name = "rbMonth";
-            this.rbMonth.Size = new System.Drawing.Size(55, 17);
+            this.rbMonth.Size = new System.Drawing.Size(103, 29);
             this.rbMonth.TabIndex = 2;
             this.rbMonth.TabStop = true;
             this.rbMonth.Text = "Month";
@@ -151,10 +152,10 @@ namespace SystemSoftwareDevProject1
             // rbWeek
             // 
             this.rbWeek.AutoSize = true;
-            this.rbWeek.Location = new System.Drawing.Point(3, 34);
-            this.rbWeek.Margin = new System.Windows.Forms.Padding(2);
+            this.rbWeek.Location = new System.Drawing.Point(6, 65);
+            this.rbWeek.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbWeek.Name = "rbWeek";
-            this.rbWeek.Size = new System.Drawing.Size(54, 17);
+            this.rbWeek.Size = new System.Drawing.Size(98, 29);
             this.rbWeek.TabIndex = 1;
             this.rbWeek.TabStop = true;
             this.rbWeek.Text = "Week";
@@ -163,10 +164,10 @@ namespace SystemSoftwareDevProject1
             // rbDay
             // 
             this.rbDay.AutoSize = true;
-            this.rbDay.Location = new System.Drawing.Point(3, 16);
-            this.rbDay.Margin = new System.Windows.Forms.Padding(2);
+            this.rbDay.Location = new System.Drawing.Point(6, 31);
+            this.rbDay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbDay.Name = "rbDay";
-            this.rbDay.Size = new System.Drawing.Size(44, 17);
+            this.rbDay.Size = new System.Drawing.Size(81, 29);
             this.rbDay.TabIndex = 0;
             this.rbDay.TabStop = true;
             this.rbDay.Text = "Day";
@@ -174,9 +175,10 @@ namespace SystemSoftwareDevProject1
             // 
             // btnGo
             // 
-            this.btnGo.Location = new System.Drawing.Point(12, 200);
+            this.btnGo.Location = new System.Drawing.Point(24, 385);
+            this.btnGo.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnGo.Name = "btnGo";
-            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.Size = new System.Drawing.Size(150, 44);
             this.btnGo.TabIndex = 21;
             this.btnGo.Text = "Go!";
             this.btnGo.UseVisualStyleBackColor = true;
@@ -185,17 +187,19 @@ namespace SystemSoftwareDevProject1
             // cbStocks
             // 
             this.cbStocks.FormattingEnabled = true;
-            this.cbStocks.Location = new System.Drawing.Point(12, 173);
+            this.cbStocks.Location = new System.Drawing.Point(24, 333);
+            this.cbStocks.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.cbStocks.Name = "cbStocks";
-            this.cbStocks.Size = new System.Drawing.Size(121, 21);
+            this.cbStocks.Size = new System.Drawing.Size(238, 33);
             this.cbStocks.TabIndex = 22;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 157);
+            this.label3.Location = new System.Drawing.Point(24, 302);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(66, 25);
             this.label3.TabIndex = 23;
             this.label3.Text = "Stock";
             // 
@@ -203,23 +207,24 @@ namespace SystemSoftwareDevProject1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsslMain});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 223);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 434);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(275, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 28, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(550, 37);
             this.statusStrip1.TabIndex = 24;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // tsslMain
             // 
             this.tsslMain.Name = "tsslMain";
-            this.tsslMain.Size = new System.Drawing.Size(16, 17);
+            this.tsslMain.Size = new System.Drawing.Size(30, 32);
             this.tsslMain.Text = "...";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(275, 245);
+            this.ClientSize = new System.Drawing.Size(550, 471);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.cbStocks);
@@ -230,6 +235,7 @@ namespace SystemSoftwareDevProject1
             this.Controls.Add(this.gbLocality);
             this.Controls.Add(this.lbToDateLabel);
             this.Controls.Add(this.lbFromDateLabel);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
